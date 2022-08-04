d42257586142774aacf078f46679524308d11a24
William Chang <wilicw1346@gmail.com>
2022-08-04 23:10:51 +0800

    Change syntax

--- gruntfile.js
+++ gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
       dist: {
         src: [
           'css/base.css',
-          'css/syntax/monokai.css',
+          'css/syntax/github.css',
           'css/octicons.css',
           'css/font-awesome.min.css'
         ],
